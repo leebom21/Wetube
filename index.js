456f406e112d3b41df3d4333fe474cb6600ce90c
leebom <d0201d@hanmail.net>
2020-11-11 21:17:28 +0900

    post/get

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hi!");
+const express = require('express')  //require는 모듈안에 express를 찾아옴
+const  app = express();
+
+const PORT = 4000;
+
+function handleLisening() {
+    console.log(`Listening on: http://localhost:${PORT}`)
+}
+
+function handleHome(req, res) {
+    console.log(req);
+    res.send('hello from home')
+}
+
+app.get("/", handleHome)   //누군가 main url로 접근할 시 handleHome 함수 호출
+
+app.listen(PORT, handleLisening);  //포트번호
+
+
