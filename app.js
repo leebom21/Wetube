e359b666c1e23ec4a432998593e0031f12adb564
leebom21 <52150668+leebom21@users.noreply.github.com>
2020-11-24 22:35:07 +0900

    MVC Pattern 01

--- app.js
+++ app.js
@@ -1,39 +1,23 @@
 //const express = require('express')  //require는 모듈안에 express를 찾아옴
 import express from "express";
-import morgan from "morgan";  //logger
+import morgan from "morgan"; //logger
 import helmet from "helmet"; // 보안을 위한
 import cookieParser from "cookie-parser";
 import bodyParser, { urlencoded } from "body-parser";
-import {userRouter} from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
+const app = express();
 
-const  app = express();
-
-const  handleHome = (req, res) =>  res.send('hello from my cccc ');
-
-const  handleProfile = (req, res) =>   res.send('your are on my profile');  
-
-// const betweenHome = (req, res, next) => {    //middleware
-//     console.log("Between") ;
-//     next();   
-// }
-
-// const middleware = (req, res, next) => {    //middleware는 중간에 연결을 끊을수 있음
-//     res.send('not happening')
-// } 
-
-// app.use(betweenHome);
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get("/", handleHome) ;  //누군가 main url로 접근할 시 handleHome 함수 호출
-
-app.get("/profile", handleProfile);
-
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter); //join, login, home, search 등을 다룸
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
-
---
+++ routes.js
@@ -0,0 +1,44 @@
+//다른 페이지에서 같은 URL를 사용해야 할수도 있기 때문에 js파일에 쪼개서 넣어주는것을 권장
+//Global
+const HOME = "/";
+const JOIN = "/join";
+const LOGIN = "/login";
+const LOGOUT = "/logout";
+const SEARCH = "/search";
+
+//Users
+
+const USER = "/users";
+const USER_DETAIL = "/:id";
+const EDIT_PROFILE = "/edit-profile";
+const CHANGE_PASSWORD = "/change-password";
+
+
+//Videos
+
+const VIDEOS = "/videos";
+const UPLOAD = "/upload";
+const VIDEO_DETAIL = "/:id"; // /:id=변하는 값일 때 작성(EXPRESS가 판단할수있어짐) //  /id=텍스트로 인식
+const EDIT_VIDEO = "/:id/edit"; //video/1/edit
+const DELETE_VIDEO = "/:id/delete";
+
+
+const routers = {
+    home: HOME,
+    join: JOIN,
+    login: LOGIN,
+    logout: LOGOUT,
+    search: SEARCH,
+    users: USERS,
+    userDetail: USER_DETAIL,
+    editProfile: EDIT_PROFILE,
+    changePassword: CHANGE_PASSWORD,
+    videos: VIDEOS,
+    upload: UPLOAD,
+    videoDetail: VIDEO_DETAIL,
+    editVideo: EDIT_VIDEO,
+    deleteVideo: DELETE_VIDEO
+};
+
+
+export default routes;
